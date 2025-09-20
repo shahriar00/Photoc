fd2a57e6db2b646c5f0793e4f946dceb26fa18f0
shahriar00 <shahriarsazid783@gmail.com>
2025-09-20 14:54:15 +0600

    Initial Commit

---
+++ Photoc/PhotocApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotocApp.swift
+//  Photoc
+//
+//  Created by Shahriar Islam Sazid on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotocTests/PhotocTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotocTests.swift
+//  PhotocTests
+//
+//  Created by Shahriar Islam Sazid on 9/20/25.
+//
+
+import Testing
+
+struct PhotocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
